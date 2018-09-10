8e62bb48a3ab3ccf0fe9e7c34f9544aba5d19da7
Ashish <ashishrodio@gmail.com>
2018-09-11 00:41:13 +0530

    Added swipe left/right console statements

--- src/Deck.js
+++ src/Deck.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { View, PanResponder, Animated, Dimensions} from 'react-native';
+import { View, PanResponder, Animated, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_THRESHOLD = SCREEN_WIDTH * 0.25;
 
 class Deck extends React.Component {
 
-    
+
     constructor(props) {
         super(props);
-    
+
         this.position = new Animated.ValueXY();
         this.panResponder = PanResponder.create({
-          onStartShouldSetPanResponder: () => true,
-          onPanResponderMove: (event, gesture) => {
-              this.position.setValue({ x: gesture.dx, y: gesture.dy });
-          },
-          onPanResponderRelease: () => { this.resetPosition() }
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                this.position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SCREEN_THRESHOLD) {
+                    console.log('swiped right!');
+                } else if (gesture.dx < -SCREEN_THRESHOLD) {
+                    console.log('swiped left!');
+                } else {
+                    this.resetPosition()
+                }
+            }
         });
-      }
+    }
 
     resetPosition() {
         Animated.spring(this.position, {
-            toValue:{ x:0, y:0 }
+            toValue: { x: 0, y: 0 }
         }).start();
     }
 
@@ -34,17 +43,17 @@ class Deck extends React.Component {
 
         return {
             ...this.position.getLayout(),
-            transform : [{ rotate }]
+            transform: [{ rotate }]
         }
     }
 
     renderCards() {
-        return this.props.data.map((card,i) => {
+        return this.props.data.map((card, i) => {
 
-            if(i === 0) {
+            if (i === 0) {
                 return (
-                    <Animated.View key={i} style={ this.getCardStyle() } { ...this.panResponder.panHandlers }>
-                        { this.props.renderCard(card) }
+                    <Animated.View key={i} style={this.getCardStyle()} {...this.panResponder.panHandlers}>
+                        {this.props.renderCard(card)}
                     </Animated.View>
                 )
             }
